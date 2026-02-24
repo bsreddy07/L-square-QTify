34ddef51f49e94d1081c200e81c9af5617d9a7d1
bsreddy07 <bssreddy565@gmail.com>
2026-02-25 03:12:44 +0530

    CSS final update

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -4,12 +4,16 @@ import headphones from "../../assets/hero_headphones.png";
 function Hero() {
   return (
     <div className={styles.hero}>
-  <div>
+  <div className={styles.textContainer}>
     <h1>100 Thousand Songs, ad-free</h1>
     <h1>Over thousands podcast episodes</h1>
   </div>
 
-  <img src={headphones} alt="headphones" />
+  <img
+    className={styles.image}
+    src={headphones}
+    alt="headphones"
+  />
 </div>
   );
 }
